Added more useful evaluation metrics like precision and recall. Additional classifier pipelines for evaluation and changed the default one. Fixed issue with classifiers that don't provide probability numbers.

classifier.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-import textutil
+import logging
 import numpy as np
+from collections import defaultdict
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB
-from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVC, SVC
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.model_selection import cross_val_score, train_test_split
+
+logger = logging.getLogger(__name__)
 
 class Classifier:
     def __init__(self, docs):
@@ -18,48 +21,33 @@ class Classifier:
             self.y.append(doc.interesting)
 
         if True in self.y and False in self.y:
-            self.pipeline = self.pipelines['MultinomialNB']
+            self.pipeline = self.pipelines['SGDClassifier.log']
             self.pipeline.fit(self.X, self.y)
 
     def predict(self, doc):
         try:
-            # example: [(False, 0.70580146379303743), (True, 0.29419853620696201)]
-            probabilities = zip(self.pipeline.classes_, self.pipeline.predict_proba([doc.classification_text])[0])
-            interesting_probability = [p[1] for p in probabilities if p[0] == True][0]
-            doc.score = interesting_probability
+            classifier = self.pipeline.named_steps['classifier']
+            if hasattr(classifier, 'predict_proba'):
+                # example: [(False, 0.70580146379303743), (True, 0.29419853620696201)]
+                probabilities = zip(self.pipeline.classes_, self.pipeline.predict_proba([doc.classification_text])[0])
+                interesting_probability = [p[1] for p in probabilities if p[0] == True][0]
+                doc.score = interesting_probability
+
             return self.pipeline.predict([doc.classification_text])[0]
-        except AttributeError:
+        except AttributeError as e:
+            logger.error(e)
             return False
 
     def eval(self):
-        CV = 10
-        scores = {}
-
-        train_sizes = np.linspace(0.05, 0.8, 16)
-        test_scores = {}
-        test_scores_x = []
+        CV = 3
+        scores = defaultdict(dict)
 
         for name, pipeline in self.pipelines.items():
-            scores[name] = cross_val_score(pipeline, self.X, self.y, cv=CV).tolist()
+            scores[name]['accuracy'] = cross_val_score(pipeline, self.X, self.y, cv=CV, scoring='accuracy').tolist()
+            scores[name]['precision'] = cross_val_score(pipeline, self.X, self.y, cv=CV, scoring='precision').tolist()
+            scores[name]['recall'] = cross_val_score(pipeline, self.X, self.y, cv=CV, scoring='recall').tolist()
 
-        for train_size in train_sizes:
-            X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, train_size=train_size, random_state=0)
-            test_scores_x.append(len(X_train))
-
-            for name, pipeline in self.pipelines.items():
-                if name not in test_scores:
-                    test_scores[name] = []
-
-                pipeline.fit(X_train, y_train)
-                test_scores[name].append(pipeline.score(X_test, y_test))
-
-        return {
-            'documents': len(self.X),
-            'scores': scores,
-            'scores_x': np.arange(CV),
-            'test_scores': test_scores,
-            'test_scores_x': test_scores_x,
-        }
+        return dict(scores)
 
     @property
     def pipelines(self):
@@ -79,6 +67,18 @@ class Classifier:
             'LinearSVC': Pipeline([
                 ('vectorizer', TfidfVectorizer()),
                 ('classifier', LinearSVC())
+            ]),
+            'SVC.linear': Pipeline([
+                ('vectorizer', TfidfVectorizer()),
+                ('classifier', SVC(kernel='linear', probability=True))
+            ]),
+            'SGDClassifier.hinge': Pipeline([
+                ('vectorizer', TfidfVectorizer()),
+                ('classifier', SGDClassifier(loss='hinge'))
+            ]),
+            'SGDClassifier.log': Pipeline([
+                ('vectorizer', TfidfVectorizer()),
+                ('classifier', SGDClassifier(loss='log'))
             ]),
             'RandomForest': Pipeline([
                 ('vectorizer',  TfidfVectorizer()),

eval.py
@@ -2,7 +2,6 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.interpolate import splrep, splev
 from dataset import DataSet
 from classifier import Classifier
 
@@ -12,29 +11,32 @@ if len(sys.argv) < 2:
 
 source = sys.argv[1]
 
-def plot_eval(ax, classifier_scores, x):
-    for name, scores in classifier_scores.items():
-        # line smoothing from https://stackoverflow.com/a/30437051/3782059
-        tck = splrep(x, scores)
-        xnew = np.linspace(x[0], x[-1])
-        ynew = splev(xnew, tck)
-        ax.plot(xnew, ynew, label=name, alpha=0.5)
-
-    ax.legend(loc='lower right')
-    ax.axis(xmin=x[0], xmax=x[-1], ymin=0, ymax=1)
-    ax.grid()
-
 dataset = DataSet(source)
-classifier = Classifier(dataset.all_documents)
+documents = dataset.all_documents
+classifier = Classifier(documents)
 evaluation = classifier.eval()
 
-for name, scores in evaluation['scores'].items():
-    print('average score {}: {}'.format(name, np.mean(scores)))
-
-fig, axes = plt.subplots(nrows=2, ncols=1)
-
+fig, ax = plt.subplots()
 plt.suptitle(source)
-plot_eval(axes[0], evaluation['scores'], evaluation['scores_x'])
-plot_eval(axes[1], evaluation['test_scores'], evaluation['test_scores_x'])
+ax.axis(xmin=-0.05, xmax=1.05, ymin=-0.05, ymax=1.05)
+ax.set_xlabel('recall')
+ax.set_ylabel('precision')
+ax.grid()
 
+linesep = '-' * 50
+
+print(linesep)
+print('number of documents: {}'.format(len(documents)))
+print(linesep)
+
+for name, scores in evaluation.items():
+    for scoring, values in scores.items():
+        print('{:20} {:10}: {:5.3f} (std={:5.3f})'.format(name, scoring, np.mean(values), np.std(values)))
+    print(linesep)
+
+    x = scores['recall']
+    y = scores['precision']
+    ax.plot(x, y, 'o', label=name, alpha=0.5)
+
+ax.legend(loc='lower right')
 plt.show()